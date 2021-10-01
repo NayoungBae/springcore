91ab786085ea57323038b824e13c8254cca47ade
baenayoung <nybae2003@naver.com>
2021-10-01 17:23:22 +0900

    로그인한 계정에 따라 등록한 상품 목록 조회

--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -1,32 +1,37 @@
 package com.sparta.springcore.service;
 
-import com.sparta.springcore.model.Product;
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
-import com.sparta.springcore.repository.ProductRepository;
 import com.sparta.springcore.dto.ProductRequestDto;
-import org.springframework.stereotype.Component;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.List;
 
-@Component
+@Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
-        Product product = new Product(requestDto);
-        return productRepository.save(product);
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
+// 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto, userId);
+
+        productRepository.save(product);
+
+        return product;
     }
 
-    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        Product product = productRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
-        );
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
+
         int myprice = requestDto.getMyprice();
         product.setMyprice(myprice);
         productRepository.save(product);
@@ -34,7 +39,8 @@ public class ProductService {
         return product;
     }
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
     }
 }
